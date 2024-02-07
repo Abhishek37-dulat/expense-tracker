371c81ca869d19afc49ce13d4eda8a2bb6ea26b4
Abhishek Dulat <abhishek11906997dulat@gmail.com>
2024-02-07 09:58:39 +0530

    expense-api

---
+++ src/main/java/org/example/services/TableService.java
@@ -0,0 +1,15 @@
+package org.example.services;
+
+import com.pairlearning.expensetrackerapi.domain.Table;
+import com.pairlearning.expensetrackerapi.exceptions.EtBadRequestException;
+import com.pairlearning.expensetrackerapi.exceptions.EtResourceNotFoundException;
+
+import java.util.List;
+
+public interface TableService {
+    List<Table> fetchAllTables();
+    Table fetchTableById(String tableType) throws EtResourceNotFoundException;
+    Table addTable(Table table) throws EtBadRequestException;
+    void updateTable(String tableType, Table table) throws EtBadRequestException;
+    void removeTableById(String tableType) throws EtResourceNotFoundException;
+}
